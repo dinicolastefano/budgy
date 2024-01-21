4c6c99be9cf10847ae48e6eb65908b10f0148612
dinicolastefano <122374188+dinicolastefano@users.noreply.github.com>
2024-01-21 19:12:09 +0100

    Update Budgy_app.py

--- Budgy_app.py
+++ Budgy_app.py
@@ -1,23 +1,30 @@
 from st_on_hover_tabs import on_hover_tabs
 import streamlit as st
+import streamlit_float # recommended
+
+# Note - on the page's first load - when user comes in from the url rather than clicking on the tab, the active page will be derived from the url or from the `loadPageName` parameter. Please make sure all params in the data array object are inputed.
+
 st.set_page_config(layout="wide")
 
-st.header("Custom tab component for on-hover navigation bar")
-st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
+streamlit_float.float_init(include_unstable_primary=False)
+
+#if the rendering gets too buggy that you don't feel comfortable, a simple time.sleep(2) helps after this component.
+time.sleep(2) # optional
+
+data_ = [
+            {"index":0, "label":"Example", "page":"example", "href":"http://localhost:8501/"},
+            {"index":1, "label":"Page", "page":"page", "icon":"ri-logout-box-r-line", "href":"http://localhost:8501/page"}
+        ]
+
+if "currentPage" not in st.session_state: # required as component will be looking for this in session state to change page via `switch_page`
+    st.session_state["currentPage"] = data_[0] 
+else:
+    st.session_state["currentPage"] = data_[0] 
 
 
-with st.sidebar:
-    tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
-                         iconName=['dashboard', 'money', 'economy'], default_choice=0)
-
-if tabs =='Dashboard':
-    st.title("Navigation Bar")
-    st.write('Name of option is {}'.format(tabs))
-
-elif tabs == 'Money':
-    st.title("Paper")
-    st.write('Name of option is {}'.format(tabs))
-
-elif tabs == 'Economy':
-    st.title("Tom")
-    st.write('Name of option is {}'.format(tabs))
+with st.container():
+    defaultSidebar = CustomSidebarDefault(closeNavOnLoad=False, backgroundColor="brown", loadPageName="example", data=data_, LocalOrSessionStorage=1, serverRendering=False, webMedium="local") 
+    defaultSidebar.load_custom_sidebar()
+    defaultSidebar.change_page()
+    
+    streamlit_float.float_parent(css="position:fixed; top:-1000px;") # gets rid of the whitespace created from the iframes used to build the component - no big forehead.
